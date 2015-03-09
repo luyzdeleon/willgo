dd7528f118bd13ec7ccd33161ade4b697c17df4b
Luis de Leon <luyzdeleon@gmail.com>
2015-03-09 18:58:20 -0400

    Fixes to create function in db layer

--- www/js/DB.js
+++ www/js/DB.js
@@ -1,70 +1,87 @@
+// Dependency to the DB Layer
+String.prototype.format = function (o) {
+    return this.replace(/{([^{}]*)}/g,
+        function (a, b) {
+            var r = o[b];
+            return typeof r === 'string' || typeof r === 'number' ? r : a;
+        }
+    );
+};
+
+var CreateTable = "CREATE TABLE IF NOT EXISTS preference(id INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT, place TEXT)";
+var SelectAll = "SELECT * FROM preference";
+var INSERT = "INSERT INTO {table}({columns}) values({wildcards})";
+var update = "UPDATE preference set name=?,place=? where id=?";
+var deletef = "DELETE from preference where id=?";
+
+if (window.openDatabase) {
+  var db = openDatabase("WilgoDB", "1.0", "test", 20000);
+} else {
+  alert("no se pudo crear la bd");
+}
+
+createTable();
 
 
 
+function createTable() {
+  db.transaction(function(tx) {
+    tx.executeSql(CreateTable, []);
+  });
+}
 
+// {key:value}
+// cb: callback function(tx, result)
+function insert(table_name, fields, cb) {
+  var keys = Object.keys(fields),
+      columns = keys.join(','),
+      values = [],
+      statement = '',
+      wildcards = [];
 
-var CreateTable="CREATE TABLE IF NOT EXISTS preference(id INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT, place TEXT)";
-var SelectAll="SELECT * FROM preference";
-var Insert="INSERT INTO preference(name, place) values(?, ?)";
-var update="UPDATE preference set name=?,place=? where id=?";
-var deletef="DELETE from preference where id=?";
+  for(key in keys){
+    values.push(fields[key]);
+    wildcards.push('?');
+  }
 
-if(window.openDatabase)var db=openDatabase("WilgoDB","1.0","test",20000);
-else alert("no se pudo crear la bd");
- 
- createTable();
-	
- 
+  wildcards = wildcards.join(',');
+  statement = INSERT.format({'columns': columns, 'wildcards': wildcards, 'table': table_name});
 
-		function createTable() {
-			db.transaction(function(tx)
-			{
-				tx.executeSql(CreateTable,[]);
-			});
-		}
+  db.transaction(function(tx) {
+    tx.executeSql(insert, values, cb);
+  });
+}
 
-		 function insert() {
-		 	var name=document.getElementById('name').value;
-			var place=document.getElementById('place').value;
-			//var coordinates=document.getElementById('coordinates').value;
-			
-		        db.transaction(function(tx) {
-		          tx.executeSql(Insert, [name, place]);
-		        });
-		      }
+function updateFill(id) {
+  db.transaction(function(tx) {
+    var name = document.getElementById('name').value;
+    var place = document.getElementById('place').value;
+    tx.executeSql(update, [name, place, id]);
+  });
+}
 
-		   function updateFill(id){
-			db.transaction(function(tx){
-			var name=document.getElementById('name').value;
-			var place=document.getElementById('place').value;
-				tx.executeSql(update,[name,place,id]);
-			});
-		}
+function deleteFill(id) {
+  db.transaction(function(tx) {
+    tx.executeSql(deletef, [id]);
+  });
+}
 
-		function deleteFill(id){
-			db.transaction(function(tx){
-				tx.executeSql(deletef,[id]);
-			});
-		}
-
-			/**
+/**
 			callback is a Function with the signature:
 			callback(elements)
 			*/
-			function read(column,callback){
+function read(columns, callback) {
 
-				db.transaction(function(tx){
-					tx.executeSql(SelectAll,[],function(tx,result){
-					var row=result.rows;
-					for(var i=0, item=null; i<row.length;i++){
-						item=row.item(i);
-						element.push(item[column]);
-					}
-					callback(element);
-				});
-			});
-				
-			
-		}
+  db.transaction(function(tx) {
+    tx.executeSql(SelectAll, [], function(tx, result) {
+      var row = result.rows;
+      for (var i = 0, item = null; i < row.length; i++) {
+        item = row.item(i);
+        element.push(item[column]);
+      }
+      callback(element);
+    });
+  });
 
-		 
+
+}
